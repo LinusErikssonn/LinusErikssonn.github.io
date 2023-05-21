04f6852ac894cc6f78bdfa169ec84da75c5787f8
LinusErikssonn <crasyagent@gmail.com>
2023-05-21 22:59:35 +0200

    wasd

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
 $("#Hem").click(function () {
   $("html,body").animate(
     {
+      scrollTop: $("main").offset().top - 200,
+    },
+    1000
+  );
+});
+
+$("#Bilder").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("main").offset().top - 100,
+    },
+    1000
+  );
+});
+
+$("#produkter").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#ruta3").offset().top - 200,
+    },
+    1000
+  );
+});
+
+$("#kampanjer").click(function () {
+  $("html,body").animate(
+    {
+      scrollTop: $("#ruta2").offset().top - 200,
+    },
+    1000
+  );
+});
+
+$("#kontakt").click(function () {
+  $("html,body").animate(
+    {
       scrollTop: $("footer").offset().top,
     },
     1000
